15e73e320fe85e4746f704e3edbea919e8a8b9e5
Xiaofeng Wang <henrywangxf@me.com>
2022-11-12 16:31:11 +0800

    proxy: Enable crb and power-tools on CS9 and CS8
    Packages python3-docutils, gpgme-devel, libassuan-devel come from
    crb repo on CS9 and powertools repo on CS8
    Those packages are required by build osbuild-composer

--- proxy/startup-script.sh
+++ proxy/startup-script.sh
@@ -9,3 +9,11 @@ if [[ "$ID" == "fedora" ]]; then
     growpart /dev/sda 5
     btrfs filesystem resize 1:+70G /
 fi
+
+# Enable CRB repo or powertools repo on Centos Stream 9 or 8
+if [[ "${ID}-${VERSION_ID}" == "centos-9" ]]; then
+    dnf config-manager --set-enabled crb
+fi
+if [[ "${ID}-${VERSION_ID}" == "centos-8" ]]; then
+    dnf config-manager --set-enabled powertools
+fi
