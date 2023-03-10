77f4efd730a061d8cdf54c6da07f01cadbca1bcb
Xiaofeng Wang <henrywangxf@me.com>
2023-03-10 11:05:19 +0800

    Move stage environment code here

---
+++ proxy-stage/startup-script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo -e 'admin\tALL=(ALL)\tNOPASSWD: ALL' >> /etc/sudoers
+
+source /etc/os-release
+
+# All Fedora GCP images do not support auto resize root disk
+if [[ "$ID" == "fedora" ]]; then
+    growpart /dev/sda 5
+    btrfs filesystem resize 1:+70G /
+fi
+
+# Enable CRB repo or powertools repo on Centos Stream 9 or 8
+if [[ "${ID}-${VERSION_ID}" == "centos-9" ]]; then
+    dnf config-manager --set-enabled crb
+fi
+if [[ "${ID}-${VERSION_ID}" == "centos-8" ]]; then
+    dnf config-manager --set-enabled powertools
+fi
