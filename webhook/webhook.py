3d713413f8b93caa8701d9e071b3e49cda159303
Xiaofeng Wang <henrywangxf@me.com>
2023-06-29 12:28:54 +0800

    webhook: Add greenboot and fido-device-onboard-rs repos

--- webhook/webhook.py
+++ webhook/webhook.py
@@ -16,7 +16,9 @@ hmac_key = bytes(GITHUB_APP_WEBHOOK_SECRET, 'UTF-8')
 ALLOWED_REPOS = [
     "virt-s1/kite-demo",
     "virt-s1/rhel-edge",
-    "coreos/coreos-installer-dracut"
+    "coreos/coreos-installer-dracut",
+    "fedora-iot/fido-device-onboard-rs",
+    "fedora-iot/greenboot"
 ]
 
 def kite_webhook_handler(event, context):
