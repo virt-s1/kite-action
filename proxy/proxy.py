aa90d07d99b3d7d1933121db30019d727ce7aae5
Xiaofeng Wang <henrywangxf@me.com>
2024-05-06 21:31:31 +0800

    proxy: add rhel 9.5 runner support
    Signed-off-by: Xiaofeng Wang <henrywangxf@me.com>

--- proxy/proxy.py
+++ proxy/proxy.py
@@ -62,7 +62,8 @@ while True:
                 "rhel-9-1",
                 "rhel-9-2",
                 "rhel-9-3",
-                "rhel-9-4"
+                "rhel-9-4",
+                "rhel-9-5"
             ]
             for i in suported_os:
                 if i in labels:
